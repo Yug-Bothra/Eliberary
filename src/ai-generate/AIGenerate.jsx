4ae89bb7d2af6016ddaf38ee49ac0d9a1fc13038
Yug Bothra <yugbothra200@outlook.com>
2025-11-17 00:51:11 +0530

    api

--- src/ai-generate/AIGenerate.jsx
+++ src/ai-generate/AIGenerate.jsx
@@ -10,7 +10,7 @@ function AIGenerate() {
   const [generated, setGenerated] = useState(false);
 
   const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-  const apiUrl = `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+ const apiUrl = `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-lite:generateContent?key=${API_KEY}`;
 
   const handleGenerate = async () => {
     if (!inputText.trim()) return;
